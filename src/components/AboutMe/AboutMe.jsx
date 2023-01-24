3d89029add8110fd2a993f54af42b2a671b41c89
Dmytro Shakh <work.dmytro02@gmail.com>
2023-01-24 19:40:42 +0100

    refactor: вынести портфолио в отдельный комонент
    1. Удалить верстку и стили из компонента AboutMe
    2. Создать компонент Portfolio
    3. Подключить компонент Portfolio в компоненте AboutMe

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -11,6 +11,7 @@ import photo from './../../images/photo_Dmytro.jpg';
 // ? из констант
 import { mainNavigationRouters as routers } from './../../utils/Constants';
 import LandingBlock from '../LandingBlock/LandingBlock';
+import Portfolio from './../Portfolio/Portfolio';
 
 function AboutMe() {
 
@@ -51,62 +52,7 @@ function AboutMe() {
         <img className='aboutMe__img' src={photo} alt="фото моей девушки" />
       </div>
 
-      <div className='aboutMe__portfolio'>
-        <h3 className='aboutMe__portfolio-title'>
-          Портфолио
-        </h3>
-
-        <div className='aboutMe__portfolio-list-web-sites'>
-
-          <div className='aboutMe__portfolio-web-site'>
-            <h4 className='aboutMe__portfolio-web-site-name'>
-              Статичный сайт
-            </h4>
-
-            <a
-              href='https://github.com/chyVacheck/how-to-learn'
-              target="_blank"
-              rel="noreferrer"
-              className='aboutMe__portfolio-web-site-arrow link'
-            >
-              ↗
-            </a>
-          </div>
-
-          <div className='aboutMe__portfolio-web-site'>
-            <h4 className='aboutMe__portfolio-web-site-name'>
-              Адаптивный сайт
-            </h4>
-
-            <a
-              href='https://chyvacheck.github.io/russian-travel/'
-              target="_blank"
-              rel="noreferrer"
-              className='aboutMe__portfolio-web-site-arrow link'
-            >
-              ↗
-            </a>
-          </div>
-
-          <div className='aboutMe__portfolio-web-site aboutMe__portfolio-web-site_border_no-border'>
-            <h4 className='aboutMe__portfolio-web-site-name'>
-              Одностраничное приложение
-            </h4>
-
-            <a
-              href='https://github.com/chyVacheck/react-mesto-api-full-gha'
-              target="_blank"
-              rel="noreferrer"
-              className='aboutMe__portfolio-web-site-arrow link'
-            >
-              ↗
-            </a>
-          </div>
-        </div>
-
-
-
-      </div>
+      <Portfolio />
 
     </LandingBlock>
   );
---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,73 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+// * react
+
+// ? стили
+import './Portfolio.css';
+
+// ? компоненты
+
+function Portfolio() {
+
+
+  return (
+    <article className={'portfolio'}>
+
+      <h3 className='portfolio__title'>
+        Портфолио
+      </h3>
+
+
+      <div className='portfolio__list-projects'>
+
+        <div className='portfolio-web-site'>
+          <h4 className='portfolio-web-site-name'>
+            Статичный сайт
+          </h4>
+
+          <a
+            href='https://github.com/chyVacheck/how-to-learn'
+            target="_blank"
+            rel="noreferrer"
+            className='portfolio-web-site-arrow link'
+          >
+            ↗
+          </a>
+        </div>
+
+        <div className='portfolio-web-site'>
+          <h4 className='portfolio-web-site-name'>
+            Адаптивный сайт
+          </h4>
+
+          <a
+            href='https://chyvacheck.github.io/russian-travel/'
+            target="_blank"
+            rel="noreferrer"
+            className='portfolio-web-site-arrow link'
+          >
+            ↗
+          </a>
+        </div>
+
+        <div className='portfolio-web-site portfolio-web-site_border_no-border'>
+          <h4 className='portfolio-web-site-name'>
+            Одностраничное приложение
+          </h4>
+
+          <a
+            href='https://github.com/chyVacheck/react-mesto-api-full-gha'
+            target="_blank"
+            rel="noreferrer"
+            className='portfolio-web-site-arrow link'
+          >
+            ↗
+          </a>
+        </div>
+      </div>
+
+    </article>
+  );
+}
+
+export default Portfolio;
