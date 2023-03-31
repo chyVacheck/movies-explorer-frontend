62123673202ffa1e87c6a1d49a1a3417a76ba058
Dmytro Shakh <work.dmytro02@gmail.com>
2023-03-31 14:59:23 +0200

    add adaptive: component AboutMe

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,59 +1,58 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-
 // * react
 
 // ? стили
-import './AboutMe.css';
+import "./AboutMe.css";
 
 // ? фото
-import photo from './../../images/photo_Dmytro.jpg';
+import photo from "./../../images/photo_Dmytro.jpg";
 
 // ? из констант
-import { mainNavigationRouters as routers } from './../../utils/Constants';
-import LandingBlock from '../LandingBlock/LandingBlock';
-import Portfolio from './../Portfolio/Portfolio';
+import { mainNavigationRouters as routers } from "./../../utils/Constants";
+
+// ? компоненты
+import LandingBlock from "../LandingBlock/LandingBlock";
+import Portfolio from "./../Portfolio/Portfolio";
 
 function AboutMe() {
-
   return (
     <LandingBlock
-      nameOfClass={'aboutMe'}
+      nameOfClass={"aboutMe"}
       setting={{
         big: true,
         grey: false,
       }}
       title={routers[2].context}
     >
+      <div className="aboutMe__info-img">
+        <div className="aboutMe__info">
+          <h3 className="aboutMe__title">Дмитрий</h3>
 
-      <div className='aboutMe__info-img'>
-        <div className='aboutMe__info'>
-          <h3 className='aboutMe__title'>
-            Дмитрий
-          </h3>
+          <h4 className="aboutMe__sub-title">Фронтенд-разработчик, 20 лет</h4>
 
-          <h4 className='aboutMe__sub-title'>
-            Фронтенд-разработчик, 20 лет
-          </h4>
-
-          <p className='aboutMe__text'>
-            Я живу в Германии. У меня есть любимое занятие и любимая девушка, она на фото справа.
-            Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2022 года я начал учиться веб-разработке на курсах от компании ЯндкексПрактикум. После того, как прошёл часть курса по веб-разработке, начал заниматься фриланс-заказами, а сейчас в поиске постоянной работы.
+          <p className="aboutMe__text">
+            Я живу в Германии. У меня есть любимое занятие и любимая девушка,
+            она на фото. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно
+            начал кодить. С 2022 года я начал учиться веб-разработке на курсах
+            от компании ЯндкексПрактикум. После того, как прошёл часть курса по
+            веб-разработке, начал заниматься фриланс-заказами, а сейчас в поиске
+            постоянной работы.
           </p>
 
           <a
-            lang='en'
-            className='aboutMe__link link'
-            href='https://github.com/chyVacheck'
-            target="_blank" rel="noreferrer" >
+            lang="en"
+            className="aboutMe__link link"
+            href="https://github.com/chyVacheck"
+            target="_blank"
+            rel="noreferrer"
+          >
             Github
           </a>
         </div>
 
-        <img className='aboutMe__img' src={photo} alt="фото моей девушки" />
+        <img className="aboutMe__img" src={photo} alt="фото моей девушки" />
       </div>
 
       <Portfolio />
-
     </LandingBlock>
   );
 }
