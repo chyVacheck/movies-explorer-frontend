ee2c95ba469a199d5861fee97b1f29af9235f845
Dmytro Shakh <work.dmytro02@gmail.com>
2023-03-31 15:00:39 +0200

    add adaptive: component Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,25 @@
-
 // * react
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 // ? стили
-import './Footer.css';
+import "./Footer.css";
 
 // ? constants
-import { YEAR, activeFooterRoutes as activ } from './../../utils/Constants.js';
+import { YEAR, activeFooterRoutes as activ } from "./../../utils/Constants.js";
 
 function Footer({ page }) {
-
   const [year, setYear] = useState(`${YEAR}`);
   const currentYear = new Date().getFullYear();
 
-  // ? отрисовка элемента 
+  // ? отрисовка элемента
   const [isActive, setIsActive] = useState(false);
 
   // отображение года
   useEffect(() => {
     if (currentYear === YEAR) {
-      setYear(currentYear)
+      setYear(currentYear);
     } else {
-      setYear(`${YEAR}-${currentYear}`)
+      setYear(`${YEAR}-${currentYear}`);
     }
   }, [currentYear]);
 
@@ -35,33 +33,33 @@ function Footer({ page }) {
   }, [page]);
 
   return (
-    isActive && <footer className={'footer'}>
-      <h6 className='footer__title'>
-        Учебный проект Яндекс.Практикум х BeatFilm.
-      </h6>
+    isActive && (
+      <footer className="footer">
+        <h6 className="footer__title">
+          Учебный проект Яндекс.Практикум х BeatFilm.
+        </h6>
 
-      <div className='footer__info'>
-        <p className='footer__info-text footer__info-text_type_copyright'>
-          © {year}
-        </p>
+        <div className="footer__info">
+          {/* © */}
+          <p className="footer__info-text footer__info-text_type_copyright">
+            © {year}
+          </p>
 
-        <p className='footer__info-text'>
-          Яндекс.Практикум
-        </p>
+          <div className="footer__info-texts">
+            <p className="footer__info-text">Яндекс.Практикум</p>
 
-        <a
-          href='https://github.com/chyVacheck'
-          className='footer__info-text link'
-          target={'_blank'}
-          rel="noreferrer"
-        >
-          Github
-        </a>
-
-      </div>
-
-
-    </footer>
+            <a
+              href="https://github.com/chyVacheck"
+              className="footer__info-text link"
+              target={"_blank"}
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </div>
+        </div>
+      </footer>
+    )
   );
 }
 
