7a40d551c83e350c324389e3b9df8f23877ac7a5
Dmytro Shakh <work.dmytro02@gmail.com>
2023-03-31 15:02:44 +0200

    add adaptive: component PageNotFound

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,33 +1,22 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-
 // * react
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 // ? стили
-import './PageNotFound.css';
+import "./PageNotFound.css";
 
 // ? константы
-import { paths } from '../../utils/Constants';
+import { paths } from "../../utils/Constants";
 
 function PageNotFound() {
-
   return (
-    <section className={'pageNotFound'}>
-
-      <div className='pageNotFound__status'>
-        <h1 className='pageNotFound__status-code'>
-          404
-        </h1>
-        <p className='pageNotFound__status-message'>
-          Страница не найдена
-        </p>
+    <section className={"pageNotFound"}>
+      <div className="pageNotFound__status">
+        <h1 className="pageNotFound__status-code">404</h1>
+        <p className="pageNotFound__status-message">Страница не найдена</p>
       </div>
 
-
-      <NavLink to={paths.aboutProject} className={'pageNotFound__link link'}>
+      <NavLink to={paths.aboutProject} className={"pageNotFound__link link"}>
         Назад
       </NavLink>
-
-
     </section>
   );
 }
