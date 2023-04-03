8a5cc444f111e86da6506d60ef1c892928beaadc
Dmytro Shakh <work.dmytro02@gmail.com>
2023-04-03 15:22:36 +0200

    feat: сделать универсальным ButtonProfile
    Теперь компонент отображается корректно в бургерном меню и в шапке профиля
    + адаптив

--- src/components/ButtonProfile/ButtonProfile.jsx
+++ src/components/ButtonProfile/ButtonProfile.jsx
@@ -8,16 +8,15 @@ import { useNavigate } from "react-router-dom";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
 // ? стили
-import './ButtonProfile.css';
+import "./ButtonProfile.css";
 
 // ? constants
-import { paths } from './../../utils/Constants.js';
+import { paths } from "./../../utils/Constants.js";
 
 // ? иконка пользователя
-import userIcon from './../../images/UserIcon.svg';
+import userIcon from "./../../images/UserIcon.svg";
 
-function ButtonProfile({ loggedIn, page }) {
-
+function ButtonProfile({ place, loggedIn }) {
   const navigate = useNavigate();
 
   // * State`s
@@ -27,35 +26,38 @@ function ButtonProfile({ loggedIn, page }) {
 
   // ? к стр авторизации
   function toLogin() {
-    navigate(paths.login)
+    navigate(paths.login);
   }
 
   // ? к стр профиля
   function toProfile() {
-    navigate(paths.profile)
+    navigate(paths.profile);
   }
 
-  return (
-    <article className={'buttonProfile'}>
-      {loggedIn ?
+  // ? className
+  const className = `button buttonProfile${
+    loggedIn ? " buttonProfile_loged_login" : ""
+  } ${place ? ` buttonProfile_place_${place}` : ""}`;
 
+  return (
+    <article className={className}>
+      {loggedIn ? (
         <button
           className="buttonProfile__enter-profile button"
           onClick={toProfile}
         >
           <p className="buttonProfile__text">Аккаунт</p>
-          <img className="buttonProfile__user-icon" src={userIcon} alt="profile" />
+          <img
+            className="buttonProfile__user-icon"
+            src={userIcon}
+            alt="profile"
+          />
         </button>
-
-        :
-
-        <button
-          className="buttonProfile__enter-login button"
-          onClick={toLogin}
-        >
+      ) : (
+        <button className="buttonProfile__enter-login button" onClick={toLogin}>
           Войти
         </button>
-      }
+      )}
     </article>
   );
 }
