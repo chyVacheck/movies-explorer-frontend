38f364ebb08b72cf99c6aa1d3c7f1a5d6617e219
Dmytro Shakh <work.dmytro02@gmail.com>
2023-04-03 15:25:12 +0200

    add component: BurgerMenu
    Компонент бургерное меню, при разрешении 1024 и ниже при авторизированном пользователе появляется возможность открыть данное меню

---
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,40 @@
+// * react
+import { useEffect, useState } from "react";
+
+// ? стили
+import "./BurgerMenu.css";
+
+// ? компоненты
+import BurgerMenuButton from "../BurgerMenuButton/BurgerMenuButton";
+import ButtonProfile from "./../ButtonProfile/ButtonProfile";
+import Navigation from "./../Navigation/Navigation";
+
+// ? constants
+
+function BurgerMenu({ isActive, setIsActive, loggedIn }) {
+  // ? отрисовка элемента
+  const [className, setClassName] = useState("");
+
+  // className
+  useEffect(() => {
+    setClassName(`BurgerMenu ${isActive ? "BurgerMenu_active_active" : ""}`);
+  }, [isActive]);
+
+  return (
+    <article className={className}>
+      <div className="BurgerMenu__button">
+        <BurgerMenuButton
+          isActive={isActive}
+          setIsActive={setIsActive}
+          loggedIn={loggedIn}
+        />
+      </div>
+
+      <Navigation place={"BurgerMenu"} loggedIn={loggedIn} />
+
+      <ButtonProfile place={"BurgerMenu"} loggedIn={loggedIn} />
+    </article>
+  );
+}
+
+export default BurgerMenu;
