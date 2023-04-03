600b9baa45512a541fbb073206bcba6e0b056be8
Dmytro Shakh <work.dmytro02@gmail.com>
2023-04-03 15:27:53 +0200

    feat + style code: добавить кнопку открытия бургерного меню
    Теперь есть кнопка открытия бургерного меню, которая появляется при разрешении 1024 и ниже, при условии что пользователь авторизован

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,35 +4,35 @@
 import { useEffect, useState } from "react";
 
 // ? стили
-import './Header.css';
+import "./Header.css";
 
 // ? компоненты
-import Logo from './../Logo/Logo';
-import Navigation from './../Navigation/Navigation';
-import ButtonProfile from './../ButtonProfile/ButtonProfile';
+import Logo from "./../Logo/Logo";
+import Navigation from "./../Navigation/Navigation";
+import ButtonProfile from "./../ButtonProfile/ButtonProfile";
+import BurgerMenuButton from "../BurgerMenuButton/BurgerMenuButton";
 
 // ? constants
-import { activeHeaderRoutes as activ } from './../../utils/Constants.js';
+import { activeHeaderRoutes as activ } from "./../../utils/Constants.js";
 
-function Header({ loggedIn, page }) {
-
+function Header({ loggedIn, page, isActiveBurgerMenu, setIsActiveBurgerMenu }) {
   // * State`s
 
   // ? className
-  const [className, setClassName] = useState('header');
-  // ? отрисовка элемента 
+  const [className, setClassName] = useState("header");
+  // ? отрисовка элемента
   const [isActive, setIsActive] = useState(false);
 
   // добавление модификаторов в зависимости от пути и loggedIn
   useEffect(() => {
-    if ((page === '/') && (loggedIn)) {
-      setClassName('header header_place_landing header_user_login');
-    } else if ((page === '/')) {
-      setClassName('header header_place_landing');
-    } else if ((loggedIn)) {
-      setClassName('header header_user_login');
+    if (page === "/" && loggedIn) {
+      setClassName("header header_place_landing header_user_login");
+    } else if (page === "/") {
+      setClassName("header header_place_landing");
+    } else if (loggedIn) {
+      setClassName("header header_user_login");
     } else {
-      setClassName('header');
+      setClassName("header");
     }
   }, [page, loggedIn]);
 
@@ -45,17 +45,25 @@ function Header({ loggedIn, page }) {
     }
   }, [page]);
 
-
   return (
-    isActive && <header className={className}>
+    isActive && (
+      <header className={className}>
+        <Logo />
 
-      <Logo />
+        <Navigation place={"header"} loggedIn={loggedIn} />
 
-      <Navigation loggedIn={loggedIn} page={page} />
+        <ButtonProfile place={"header"} loggedIn={loggedIn} />
 
-      <ButtonProfile loggedIn={loggedIn} page={page} />
-
-    </header>
+        {loggedIn && (
+          <BurgerMenuButton
+            isActive={isActiveBurgerMenu}
+            setIsActive={setIsActiveBurgerMenu}
+            place={"header"}
+            loggedIn={loggedIn}
+          />
+        )}
+      </header>
+    )
   );
 }
 
