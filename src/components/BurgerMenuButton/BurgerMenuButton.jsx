7c2d79bfe12add0d98c78287d6a8172d2177b9f6
Dmytro Shakh <work.dmytro02@gmail.com>
2023-04-03 15:23:36 +0200

    add component: BurgerMenuButton
    Кнопка для открытия/закрытия бургерного меню

---
+++ src/components/BurgerMenuButton/BurgerMenuButton.jsx
@@ -0,0 +1,32 @@
+// * react
+
+// ? стили
+import "./BurgerMenuButton.css";
+
+// ? компоненты
+
+// ? constants
+
+function BurgerMenuButton({ loggedIn, isActive, place, setIsActive }) {
+  // ? className
+  const className = `button BurgerMenuButton${
+    place ? ` BurgerMenuButton_place_${place}` : ""
+  } ${loggedIn ? "BurgerMenuButton_loged_login" : ""} ${
+    isActive ? "BurgerMenuButton_active_active" : ""
+  }`;
+
+  return (
+    <button
+      onClick={() => {
+        setIsActive(!isActive);
+      }}
+      className={className}
+    >
+      <span className="BurgerMenuButton__strip"></span>
+      <span className="BurgerMenuButton__strip"></span>
+      <span className="BurgerMenuButton__strip"></span>
+    </button>
+  );
+}
+
+export default BurgerMenuButton;
