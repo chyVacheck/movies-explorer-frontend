43b3bfb793ecf1f1b0542b8166ef8fcf85d8cdea
Dmytro Shakh <work.dmytro02@gmail.com>
2023-04-07 14:41:59 +0200

    add component: SearchForm
    1. Создать компонент Поисковой формы
    2. Сделать адаптивную верстку к компоненту

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,68 @@
+// * react
+import React, { useState } from 'react';
+// ? стили
+import './SearchForm.css';
+
+// ? компоненты
+
+// ? images
+import icon from '../../images/searchIcon.svg';
+
+function SearchForm() {
+  // * State`s
+  // ? пользователь данные
+  const [isActiveShortFilm, setIsActiveShortFilm] = useState(false);
+
+  return (
+    <article className='SearchForm'>
+      <div className='SearchForm__container'>
+        <div className='SearchForm__field-input'>
+          <img src={icon} alt='search' />
+          <input
+            type='text'
+            placeholder='Фильм'
+            className='SearchForm__input'
+          />
+          <div className='SearchForm__settings-and-button-search'>
+            <button
+              aria-label='search'
+              type='submit'
+              onClick={() => console.log(123)}
+              className='button SearchForm__button-search'
+            >
+              Найти
+            </button>
+            <div className='SearchForm__settings SearchForm__settings_place_field'>
+              <button
+                aria-label='short films'
+                type='button'
+                onClick={() => setIsActiveShortFilm(!isActiveShortFilm)}
+                className={`button SearchForm__button-short-film ${
+                  isActiveShortFilm
+                    ? 'SearchForm__button-short-film_active_active'
+                    : ''
+                }`}
+              />
+              <p className='SearchForm__text'>Короткометражки</p>
+            </div>
+          </div>
+        </div>
+        <div className='SearchForm__settings SearchForm__settings_place_container'>
+          <button
+            aria-label='short films'
+            type='button'
+            onClick={() => setIsActiveShortFilm(!isActiveShortFilm)}
+            className={`button SearchForm__button-short-film ${
+              isActiveShortFilm
+                ? 'SearchForm__button-short-film_active_active'
+                : ''
+            }`}
+          />
+          <p className='SearchForm__text'>Короткометражки</p>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default SearchForm;
