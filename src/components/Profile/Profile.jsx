4715d999b6bfdc010f5c3dfdd1e7e1143946da92
Dmytro Shakh <work.dmytro02@gmail.com>
2023-04-13 19:42:35 +0200

    feat: сделать кнопку сохранения данных
    1. Сделать кнопку сохранения данных
    2. Сделать стили для отключенной кнопки

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 // * react
-import React from 'react';
+import React, { useState } from 'react';
 
 // ? стили
 import './Profile.css';
@@ -10,7 +10,14 @@ import './Profile.css';
 import { CurrentUserContext } from './../../contexts/CurrentUserContext';
 
 function Profile({ onSubmit = () => console.log('Данные измененны') }) {
+  // ? сontext
   const user = React.useContext(CurrentUserContext);
+
+  // * State`s
+  // ? пользовательские данные
+  const [isActiveError, setIsActiveError] = useState(true);
+  const [isActiveManagement, setIsActiveManagement] = useState(true);
+  const [isButtonSaveDisabled, setIsButtonSaveDisabled] = useState(false);
 
   return (
     <section className='profile'>
@@ -20,31 +27,68 @@ function Profile({ onSubmit = () => console.log('Данные измененны
       <form onSubmit={onSubmit} className='profile__form'>
         <div className='profile__field'>
           <h2 className='profile__field-name'>Имя</h2>
-          <input value={user.name} className='profile__field-input' />
+          <input
+            onChange={() => console.log()}
+            value={user.name}
+            className='profile__field-input'
+          />
         </div>
         <div className='profile__field'>
           <h2 lang='en' className='profile__field-name'>
             E-mail
           </h2>
           <input
+            onChange={() => console.log()}
             value={user.email}
             type='email'
             className='profile__field-input'
           />
         </div>
       </form>
-      {/* // ? управление */}
-      <div className='profile__management'>
-        <button type='button' className='button profile__management-button'>
-          Редактировать
-        </button>
-        <button
-          type='button'
-          className='button profile__management-button profile__management-button_type_out'
-        >
-          Выйти из аккаунта
-        </button>
-      </div>
+
+      {/* // ? управление или ошибка и кнопка сохранить */}
+      {isActiveManagement ? (
+        <div className='profile__management'>
+          <button
+            onClick={() => setIsActiveManagement(!isActiveManagement)}
+            type='button'
+            className='button profile__management-button'
+          >
+            Редактировать
+          </button>
+          <button
+            type='button'
+            className='button profile__management-button profile__management-button_type_out'
+            onClick={() => console.log(123)}
+          >
+            Выйти из аккаунта
+          </button>
+        </div>
+      ) : (
+        <div className='profile__error-button-save'>
+          <p className='profile__error'>
+            {isActiveError && 'Это просто стилизованный пример ошибки'}
+          </p>
+          {/* // ? сохранить */}
+          <button
+            disabled={isButtonSaveDisabled}
+            type='button'
+            className={`button profile__button-save${
+              isButtonSaveDisabled
+                ? ' profile__button-save_enabled_disabled button_enabled_disabled'
+                : ''
+            }`}
+            onClick={() => {
+              console.log(
+                'Пока что нажатие диактивирует кнопку, потом это будет поменяно на логику',
+              );
+              setIsButtonSaveDisabled(true);
+            }}
+          >
+            Сохранить
+          </button>
+        </div>
+      )}
     </section>
   );
 }
